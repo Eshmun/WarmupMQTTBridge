168782f85a8f9edab103b69cfe240141c67c144f
Steven <427840@student.saxion.nl>
2019-09-22 15:42:51 +0200

    update main
    added main structure

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 import warmup4ie
 import paho.mqtt.client as mqtt
+import threading
+import json
 
 import secrets
+
+updateTime = 10
 
 device = warmup4ie.Warmup4IEDevice(secrets.email, secrets.password,
                                    secrets.location, secrets.room, 21)
 
+client = mqtt.Client("WarmupBridge")
+client.connect("test.mosquitto.org")
 
+client.subscribe("climate/warmup/temperature/set")
+client.subscribe("climate/warmup/mode/set")
+
+
+def on_message(client, userdata, message):
+    print(message.payload)
+
+
+def update():
+    t = threading.Timer(updateTime, update)
+    t.start()
+    current_temperature = device.get_current_temmperature()
+    target_temperature = device.get_target_temmperature()
+    run_mode = device.get_run_mode()
+    data_out = {"available": "online",
+                "current_temperature": current_temperature,
+                "target_temperature": target_temperature,
+                "run_mode": run_mode}
+    client.publish("climate/warmup", json.dumps(data_out))
+    print("hello, world")
+
+
+t = threading.Timer(updateTime, update)
+t.start()
+
+client.on_message = on_message
+
+
+while 1:
+    client.loop_forever()
 
 print(device.get_current_temmperature())
 print(device.get_target_temmperature())
