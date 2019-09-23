50dc252d888103e7c64857ee3768d57b23868d6d
Steven <427840@student.saxion.nl>
2019-09-23 12:08:17 +0200

    refactoring

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import json
 
 import secrets
 
-updateTime = 10
+UPDATE_INTERVAL = 10
+TEMPERATURE_COMMAND_TOPIC = "climate/warmup/temperature/set"
+MODE_COMMAND_TOPIC = "climate/warmup/mode/set"
+DATA_UPDATE_TOPIC = "climate/warmup"
 
 device = warmup4ie.Warmup4IEDevice(secrets.email, secrets.password,
                                    secrets.location, secrets.room, 21)
@@ -13,15 +16,15 @@ device = warmup4ie.Warmup4IEDevice(secrets.email, secrets.password,
 client = mqtt.Client("WarmupBridge")
 client.connect("test.mosquitto.org")
 
-client.subscribe("climate/warmup/temperature/set")
-client.subscribe("climate/warmup/mode/set")
+client.subscribe(TEMPERATURE_COMMAND_TOPIC)
+client.subscribe(MODE_COMMAND_TOPIC)
 
 
 def on_message(client, userdata, message):
-    if message.topic == "climate/warmup/temperature/set":
+    if message.topic == TEMPERATURE_COMMAND_TOPIC:
         device.set_new_temperature(int(message.payload))
 
-    if message.topic == "climate/warmup/mode/set":
+    if message.topic == MODE_COMMAND_TOPIC:
         if message.payload == "off":
             device.set_location_to_off()
 
@@ -31,28 +34,28 @@ def on_message(client, userdata, message):
         elif message.payload == "auto":
             device.set_temperature_to_auto()
 
+    print(message.topic)
     print(message.payload)
 
 
 def update():
-    t = threading.Timer(updateTime, update)
+    t = threading.Timer(UPDATE_INTERVAL, update)
     t.start()
     current_temperature = device.get_current_temmperature()
     target_temperature = device.get_target_temmperature()
     run_mode = device.get_run_mode()
-    data_out = {"available":            "online",
-                "current_temperature":  current_temperature,
-                "target_temperature":   target_temperature,
-                "run_mode":             run_mode}
-    client.publish("climate/warmup", json.dumps(data_out))
+    data_out = {"available": "online",
+                "current_temperature": current_temperature,
+                "target_temperature": target_temperature,
+                "run_mode": run_mode}
+    client.publish(DATA_UPDATE_TOPIC, json.dumps(data_out))
     print("hello, world")
 
 
-t = threading.Timer(updateTime, update)
+t = threading.Timer(UPDATE_INTERVAL, update)
 t.start()
 
 client.on_message = on_message
-
 
 while 1:
     client.loop_forever()
